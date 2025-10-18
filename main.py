96ac319de0c228310291c70957bf1e06b1b2e03a
LauTroxi <lautaroelcrack177@gmail.com>
2025-10-18 13:25:35 -0300

    Primer commit base de Menu

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("Hello Word")
-print("Hola")
+# Primer inicio de Menu, base.
+def main():
+    print("=== SISTEMA DE GESTIÓN DE PAÍSES ===")
+    print("1. Buscar país por nombre")
+    print("2. Filtrar por continente")
+    print("3. Salir")
+    
+    opcion = input("Seleccione una opción: ")
+    print(f"Opción seleccionada: {opcion}")
+
+if __name__ == "__main__":
+    main()
